dcd8ba3ad51567165a461338f8c7223ed5bd2639
Eben Burger <eben@arlowconsulting.co.za>
2021-05-18 13:59:49 +0200

    section component updated & added to home, custom.css keyframe animation added.

--- Tesla-Clone/Tesla-Clone.WebClient/ClientApp/src/components/section.js
+++ Tesla-Clone/Tesla-Clone.WebClient/ClientApp/src/components/section.js
@@ -1,11 +1,28 @@
 ﻿import React, {} from 'react';
 import styled from 'styled-components';
 
-const Section = (props) => {
+const Section = ({ title, description, backgroundImg, leftBtnText, rightBtnText }) => {
     return (
         <>
-            <Wrap>
-                Section
+            <Wrap bgImage={backgroundImg}>
+                <ItemText>
+                    <h1>{ title }</h1>
+                    <p>{ description }</p>
+                </ItemText>
+                <Buttons>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftBtnText}
+                        </LeftButton>
+                        {/*only if there is text in the right button display else don't*/}
+                        { rightBtnText &&
+                        <RightButton>
+                            {rightBtnText}
+                        </RightButton>
+                        }
+                    </ButtonGroup>
+                    <DownArrow src='/images/down-arrow.svg' alt='down arrow'/>  
+                </Buttons>
             </Wrap>
         </>
     );
@@ -14,6 +31,63 @@ const Section = (props) => {
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-image: ${props => `url('/images/${props.bgImage}')`};
+    background-repeat: no-repeat; 
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+    
+    @media (max-width: 768px) {
+        flex-direction: column;
+        align-items: center;
+    }
+`;
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    border-radius: 100px;
+    opacity: 0.856;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin:8px;
+`;
+
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.65;
+    color: black;
+`;
+
+const DownArrow = styled.img`
+    margin-top: 2px;
+    height: 40px;
+    animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
 `;
 
 export default Section;
